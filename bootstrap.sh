720ccd051528e4d977b9aa830b306be0d452e570
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-02-15 11:14:10 -0800

    Drop cloudflared

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,7 +44,7 @@ sudo nmcli device wifi connect "$SSID" password "$PSK"
 sleep 10 # wait for wifi to connect
 
 sudo apt install -y \
-    bolt cloudflared nvidia-driver systemd-zram
+    bolt nvidia-driver systemd-zram
 
 # install config files
 sudo cp -rvf --no-preserve=mode,ownership root/etc/* /etc/
