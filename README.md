98cb92025f47c10652297417d3688a49eaed1144
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-02-14 21:57:48 -0800

    Carry over the bare essentials from server-dotfiles

---
+++ README.md
@@ -0,0 +1,32 @@
+# staging-dotfiles
+
+Oh, I'll end up nuking my staging server for sure, so this repo documents everything I need to set it up from scratch, including a bootstrap script and a recovery script.
+
+## Pre-install
+
+0. Enable Wake On AC and set a battery charge limit at 70%
+
+## Install
+
+1. Booting from the install disk for Debian 12 (non-free drivers now included by default), proceed through the non-graphical install process.
+    * Time zone, keyboard, and language are self-explanatory
+    * Disable the root user (leave the root password empty)
+    * The hostname should be `kenny-staging`
+    * Nuke all partitions, single-partition install, and no swap partition (delete it after guided partitioning but before continuing, proceed through the warning about not designating swap)
+    * Turn off all desktop environments and turn on the SSH server
+
+## Post-install
+
+2. Install `git` with the command `sudo apt install git`
+
+3. Clone this repository with the command `git clone https://github.com/colonelwatch/staging-dotfiles .dotfiles`, call `cd .dotfiles && bash ./bootstrap.sh`
+
+4. Authorize thunderbolt dock through `boltctl`
+
+5. Restart
+
+## Post-bootstrap
+
+6. SSH with tunneling into the server by running the command `ssh -L 53682:localhost:53682 kenny@kenny-server` on another machine with a web browser
+
+7. Call `cd .dotfiles && bash ./recovery.sh`, which includes manual prompts and recovery
---
+++ bootstrap.sh
@@ -0,0 +1,81 @@
+# <SETUP>
+
+# check if pwd is ~/.dotfiles
+if [ ! "$PWD" = "$HOME/.dotfiles" ]; then
+    echo "Please run this script from the ~/.dotfiles directory."
+    exit 1
+fi
+
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+
+# add zram-generator repo
+sudo wget https://nabijaczleweli.xyz/pgp.txt -O /etc/apt/keyrings/nabijaczleweli.asc
+echo 'deb [signed-by=/etc/apt/keyrings/nabijaczleweli.asc] https://debian.nabijaczleweli.xyz bookworm main' | sudo tee -a /etc/apt/sources.list.d/zram-generator.list
+echo 'deb-src [signed-by=/etc/apt/keyrings/nabijaczleweli.asc] https://debian.nabijaczleweli.xyz bookworm main' | sudo tee -a /etc/apt/sources.list.d/zram-generator.list
+
+# update sources.list
+sudo cp root/etc/apt/sources.list /etc/apt/sources.list
+
+sudo apt update && sudo apt upgrade -y
+
+# </SETUP>
+
+
+
+# <ROOT>
+
+# record wifi config from /etc/network/interfaces
+SSID=$(sudo cat /etc/network/interfaces | grep wpa-ssid | sed 's/\twpa-ssid *//')
+PSK=$(sudo cat /etc/network/interfaces | grep wpa-psk | sed 's/\twpa-psk *//')
+
+# install NetworkManager and resolved (for mDNS features) and stop networking.service from using the wifi
+sudo apt install -y network-manager systemd-resolved
+sudo cat /etc/network/interfaces | head -8 > ./interfaces # prepare interfaces file without wifi config
+cat ./interfaces | sudo tee /etc/network/interfaces
+rm ./interfaces # remove interfaces file
+sudo systemctl restart networking wpa_supplicant
+sudo systemctl restart NetworkManager systemd-resolved
+sudo nmcli radio wifi off
+sudo nmcli radio wifi on
+sleep 10 # wait for wifi to be ready
+
+# connect it to the previously recorded wifi network
+sudo nmcli device wifi connect "$SSID" password "$PSK"
+sleep 10 # wait for wifi to connect
+
+sudo apt install -y \
+    bolt cloudflared nvidia-driver systemd-zram
+
+# install config files
+sudo cp -rvf --no-preserve=mode,ownership root/etc/* /etc/
+
+# use the new config files
+sudo update-grub
+sudo systemctl restart systemd-logind
+sudo systemctl restart NetworkManager
+
+# </ROOT>
+
+
+
+# <USER>
+
+sudo apt install -y \
+    build-essential ffmpeg fish htop parallel pkg-config rsync screen vim
+
+# download and execute miniconda install script
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3_install.sh
+bash ~/miniconda3_install.sh -b # conda will soon be intialized by importing the fish config
+rm ~/miniconda3_install.sh
+
+# install config files
+mkdir -p ~/.config
+ln -s -f $PWD/config/* ~/.config/
+
+# </USER>
+
+
+
+# <CLEANUP>
+
+# </CLEANUP>
